3d7740776ccb24857d7f46dd6e2eebc7e2b917f6
tonoka <vuducanh061020@gmail.com>
2021-07-02 21:00:02 +0700

    fix all

--- application/models/Mdk_minhchung.php
+++ application/models/Mdk_minhchung.php
@@ -1,44 +1,57 @@
 <?php 
 class Mdk_minhchung extends My_Model
 {
-    public function __construct()
-		{
-			parent::__construct();
-		}
         
-        public function getChuongtrinh($date){
+        // public function getChuongtrinh($date){
             
-            $this->db->select("*")
-                    ->where('dThoiGIanBD <=', $date)
-                    ->where('dThoiGIanKT >=', $date)
-                    ->order_by("PK_sMaChuongTrinh asc");
-            $res = $this->db->get("tbl_chuongtrinh")->result_array();
-            return $res;
-        }
-        public function dieukien($masv,$mact,$date){
-            $this->db->select("*")
-                    ->join("tbl_chuongtrinh","FK_sMaCT=PK_sMaChuongTrinh")
-                    ->where('dThoiGIanBD <=', $date)
-                    ->where('dThoiGIanKT >=', $date)
-                    ->where('FK_sMaSV ', $masv)
-                    ->where('PK_sMaChuongTrinh ', $mact)
-                    ->order_by("PK_sMaChuongTrinh asc");
+        //     $this->db->select("*")
+        //             ->where('dThoiGIanBD <=', $date)
+        //             ->where('dThoiGIanKT >=', $date)
+        //             ->order_by("PK_sMaChuongTrinh asc");
+        //     $res = $this->db->get("tbl_chuongtrinh")->result_array();
+        //     return $res;
+        // }
+        // public function dieukien($masv,$mact,$date){
+        //     $this->db->select("*")
+        //             ->join("tbl_chuongtrinh","FK_sMaCT=PK_sMaChuongTrinh")
+        //             ->where('dThoiGIanBD <=', $date)
+        //             ->where('dThoiGIanKT >=', $date)
+        //             ->where('FK_sMaSV ', $masv)
+        //             ->where('PK_sMaChuongTrinh ', $mact)
+        //             ->order_by("PK_sMaChuongTrinh asc");
             
-            $res = $this->db->get("tbl_minhchung")->result_array();
-            return $res;
+        //     $res = $this->db->get("tbl_minhchung")->result_array();
+        //     return $res;
+        // }
+        // public function insertMinhchung($masv,$minhchung){
+        //     $this->db->where('FK_sMaSV',$masv);
+        //     $res =$this->db->insert("tbl_minhchung",$minhchung);
+        //     return $this->db->affected_rows();
+        // }
+        // public function updateMinhchung($mamc,$duongdan){
+        //     $this->db->where('PK_sMaMC',$mamc);
+        //     $this->db->set('tLink',$duongdan);
+		// 	$this->db->update("tbl_minhchung");
+            
+        //     return $this->db->affected_rows();
+        // } 
+        public function getChuongTrinh(){
+            $this->db->select("*")
+                        ->where('dThoiGIanBD <=', date('Y-m-d'))
+                        ->where('dThoiGIanKT >=', date('Y-m-d'))
+                        ->order_by("PK_sMaChuongTrinh asc");
+            return $this->db->get("tbl_chuongtrinh")->result_array();
         }
-        public function insertMinhchung($masv,$minhchung){
-            $this->db->where('FK_sMaSV',$masv);
-            $res =$this->db->insert("tbl_minhchung",$minhchung);
+        public function insertMinhChung($data)
+        {
+            $this->db->insert('tbl_minhchung',$data);
             return $this->db->affected_rows();
         }
-        public function updateMinhchung($mamc,$duongdan){
-            $this->db->where('PK_sMaMC',$mamc);
-            $this->db->set('tLink',$duongdan);
-			$this->db->update("tbl_minhchung");
-            
-            return $this->db->affected_rows();
-        } 
-        
+        public function findMC($data)
+        {
+            $res = $this->db->where('PK_sMaMC',$data['PK_sMaMC'])
+                    ->get('tbl_minhchung')->result_array();
+                    return count($res);
+        }
 }
 ?>
--- application/views/Vdk_minhchung.php
+++ application/views/Vdk_minhchung.php
@@ -4,7 +4,66 @@
         <li class="breadcrumb-item active" aria-current="page">Thêm minh chứng</li>
     </ol>
 </nav>
-<br>
+<form class="container-fluid" method="post">
+    <div class="card my-3">
+        <div class="card-header text-center text-white bg-darkblue">
+            <h4 class="m-0">ĐĂNG KÝ MINH CHỨNG</h4>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="form-group col-xl-6">
+                    <label for="chuongtrinh">Chương trình lưu minh chứng:</label>
+                    <select name="minhchung[chuongtrinh]" id="chuongtrinh" class="form-control">
+                        <option value="0" readonly hidden>--Chọn chương trình--</option>
+                        {foreach $chuongtrinh as $k => $v}
+                        <option value="{$v.PK_sMaChuongTrinh}">{$v.sTenCT}</option>
+                        {/foreach}
+                    </select>
+                </div>
+                <div class="form-group col-xl-6">
+                    <label for="linkdrive">Link drive <small>(link minh chứng phải để chế độ chia sẻ để mọi người xem được):</small></label>
+                    <input name="minhchung[linkdrive]" id="linkdrive" type="text" class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="card-footer text-right">
+            <button class="btn btn-success" type="submit" name="minhchung[type]" value="submit">Lưu minh chứng</button>
+            <button class="btn btn-warning" type="submit" name="minhchung[type]" value="fix">Sửa minh chứng</button>
+        </div>
+    </div>
+    <div class="card my-3">
+        <div class="card-header text-center text-white bg-darkblue">
+            <h4 class="m-0">Danh mục minh chứng</h4>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped table-hover table-bordered">
+                    <thead class="text-center">
+                        <th width="10px">STT</th>
+                        <th width="20%">Tên chương trình</th>
+                        <th width="30%">Mô tả</th>
+                        <th>Thời gian bắt đầu</th>
+                        <th>Thời gian kết thúc</th>
+                        <th width="200px">Tác vụ</th>
+                    </thead>
+                    <tbody>
+                        <td class="text-center">1</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td class="text-center">
+                            <button class="btn btn-danger" type="submit" title="Xóa minh chứng"><i class="fas fa-trash"></i></button>
+                            <button class="btn btn-warning" type="button" title="Sửa minh chứng"><i class="fas fa-user"></i></button>
+                            <a class="btn btn-info" target="_" href="#" title="Xem minh chứng"><i class="fas fa-eye"></i></a>
+                        </td>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</form>
+<!-- <br>
 <br>
 <div class="container">
     <form method="post" enctype='multipart/form-data'>
@@ -52,4 +111,4 @@
 
         </div>
     </form>
-</div>
+</div> -->
