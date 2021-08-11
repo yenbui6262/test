d82b950adf2baefabb6f9d4a85d83da2d9843b50
tonoka <vuducanh061020@gmail.com>
2021-08-11 08:23:06 +0700

    merge

--- public/script/xacnhanthamgia.js
+++ public/script/xacnhanthamgia.js
@@ -1,14 +1,14 @@
-url=link_url;
+url = link_url;
 $(document).ready(function() {
     // xu ly khi nguoi dunng an duyet hoac bo duyet
     // ajax
     var check = true;
-    $("#xacnhan").on("click",".check",function(e){
+    $("#xacnhan").on("click", ".check", function(e) {
         e.preventDefault();
-        if(check){
+        if (check) {
             check = false;
             const self = $(this);
-            if(self.hasClass("btn-success")){
+            if (self.hasClass("btn-success")) {
                 $(this).removeClass("btn-success");
                 // $(this).attr("title","Hủy xác nhận");
                 self.html('<img src="' + url + 'public/images/spinner.gif">');
@@ -25,12 +25,12 @@ $(document).ready(function() {
                         id: update,
                         trangthai: "2",
                     }
-                }).done(function(data){
+                }).done(function(data) {
                     // alert("chuyển thành btn-danger");
-                    $(".badge").eq(id).toggle("slow", function(){
+                    $(".badge").eq(id).toggle("slow", function() {
                         $(".badge").eq(id).removeClass("badge-danger").addClass("badge-success");
                         $(".badge").eq(id).text("Tham gia");
-                        $(".badge").eq(id).toggle("slow",()=>{
+                        $(".badge").eq(id).toggle("slow", () => {
                             check = true;
                             self.addClass("btn-danger");
                             self.html("<i class='fa fa-user-slash'></i>");
@@ -38,28 +38,29 @@ $(document).ready(function() {
                     });
                     $(".lydo").eq(id).text("");
                 });
-            }else{
+            } else {
                 Swal.fire({
                     title: 'Lý do',
                     input: 'text',
                     inputAttributes: {
-                      autocapitalize: 'off'
+                        autocapitalize: 'off'
                     },
                     showCancelButton: true,
                     confirmButtonText: 'Lưu',
                     showLoaderOnConfirm: true,
-                    
-                  }).then((result) => {
+
+                }).then((result) => {
 
                     if (result.isConfirmed) {
                         $(this).removeClass("btn-danger");
-                        $(this).attr("title","Tham gia");
+                        $(this).attr("title", "Tham gia");
                         self.html('<img src="' + url + 'public/images/spinner.gif">');
                         var id = $(this).attr("data-id");
                         var update = $(this).attr("data-update");
                         var lydo = result.value;
                         $(".b1").eq(id).hide();
                         // $(this).removeClass("b2").addClass("b1");
+                        console.log(lydo);
                         $.ajax({
                             url: window.location.pathname,
                             type: 'POST',
@@ -70,28 +71,27 @@ $(document).ready(function() {
                                 trangthai: "3",
                                 lydo: lydo,
                             }
-                        }).done(function(data){
+                        }).done(function(data) {
                             // alert("chuyển thành btn-success");
                             // alert(lydo);
-                            $(".badge").eq(id).toggle("slow", function(){
+                            $(".badge").eq(id).toggle("slow", function() {
                                 $(".badge").eq(id).removeClass("badge-success").addClass("badge-danger");
                                 $(".badge").eq(id).text("Không tham gia");
-                                $(".badge").eq(id).toggle("slow", e =>{
+                                $(".badge").eq(id).toggle("slow", e => {
                                     check = true;
                                     self.addClass("btn-success");
                                     self.html("<i class='fa fa-user-check'></i>");
                                 });
-                                
+
                             });
                             $(".lydo").eq(id).text(lydo);
                         });
                     }
-                  })
-                  check= true;
+                })
+                check = true;
             }
-                
-            
-        }/*end if*/
+
+
+        } /*end if*/
     });
 });
-
