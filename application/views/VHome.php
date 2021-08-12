71d842b3394017b833b65129173310944757901c
qbv2001 <huutinh999999@gmail.com>
2021-08-13 01:01:51 +0700

    fix

---
+++ application/views/VHome.php
@@ -0,0 +1,20 @@
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="">Trang chủ</a></li>
+    </ol>
+</nav>
+<br>
+<div class="container-fluid">
+    <div class="card">
+        <div class="container-fluid" style="padding:30px;">
+            <div class="col-md-12 col-sm-10 col-xs-12">
+                <div class="d-table">
+                    <div class="d-cell text-center" style="padding-left:20px;">
+                        <h4><strong style="color:#0e4b90;">HELLO {if !empty($session['sHoten'])}{$session['sHoten']}!{/if}</strong></h4>
+                        <h4><strong style="color:#0e4b90;">CHÀO MỪNG ĐẾN VỚI HỆ THỐNG MINH CHỨNG-TƯ VẤN KHOA KINH TẾ TRƯỜNG ĐẠI HỌC MỞ HÀ NỘI</strong></h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
